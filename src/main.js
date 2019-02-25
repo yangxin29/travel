063eb3086c73f4426366326334a4c825fb9ce711
yangxin <904964305@qq.com>
2019-02-25 09:45:20 +0800

    项目初始化完成

--- src/main.js
+++ src/main.js
@@ -3,8 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 引入fastclick 解决移动端click事件300毫秒延时问题
+import fastClick from 'fastclick'
+
+
+
+// 引入清除默认样式的css文件 reset.css
+import './assets/styles/reset.css'
+// 解决一像素边框的问题
+import './assets/styles/border.css'
+
+
 
 Vue.config.productionTip = false
+// 将fastclick 绑定到body上
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// 引入首页组件
+import Home from '@/pages/home/Home.vue'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name:'Home',
+      component:Home,
     }
   ]
 })
