70a465cd45f4243591fd1b169d93ed6248bf62c0
yangxin <904964305@qq.com>
2019-02-27 12:08:29 +0800

    city-search

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,15 +2,20 @@
     <div>
         <!-- 城市信息页面头部 城市管理部分 -->
          <city-header></city-header>
+         <!-- 城市信息页面 搜索框组件 -->
+         <city-search></city-search>
     </div>
 </template>
 
 <script>
 // 引入 CityHeader 组件 (头部组件)
 import CityHeader from './components/Header.vue'
+// 引入 CitySearch 组件（搜索框组件）
+import CitySearch from './components/Search.vue'
     export default {
         components:{
             CityHeader,
+            CitySearch,
         }
     }
 </script>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="search">
+       <input class="search-input" type="text" placeholder="请输入城市名称或拼音">
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+
+.search {
+  height: 0.72rem;
+  padding: 0.1rem;
+  background: $bgColor;
+
+  .search-input {
+    width: 100%;
+    height: 0.62rem;
+    padding: 0  0.2rem;
+    line-height: 0.62rem;
+    text-align: center;
+    color: #666;
+    font-size:.26rem
+    border-radius: 0.06rem;
+    box-sizing: border-box;
+  }
+}
+</style>
